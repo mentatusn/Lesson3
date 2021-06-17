a735260692049fd6c48a8f676e61e1e72356d778
Andej Nesterenko <mentatus.n@gmail.com>
2021-06-17 21:20:03 +0300

    на всякий случай

--- app/src/main/java/keyone/keytwo/lesson3/MainActivity.java
+++ app/src/main/java/keyone/keytwo/lesson3/MainActivity.java
@@ -3,6 +3,7 @@ package keyone.keytwo.lesson3;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -11,6 +12,9 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
-    
 
+
+    public void button1_onClick(View view) {
+        
+    }
 }
